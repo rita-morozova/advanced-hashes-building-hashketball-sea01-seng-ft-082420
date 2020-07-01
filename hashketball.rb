f6b4897fd64c4f8da76782aaa0d32f7b14048ae8
Margarita Morozova <ritamorozova@yahoo.com>
2020-07-01 00:34:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,17 @@
-# Write your code here!
+game_hash = {
+  :home => {
+    :team_name => "",
+    :colors => [],
+    :players =>[{
+      :player_name =>
+      :number =>
+      :shoe =>
+      :points =>
+      :rebounds =>
+      :assists =>
+      :steals =>
+      :blocks
+    }]
+  },
+  :away => {}
+}
